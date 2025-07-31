cd958844ed6476cbf9bbaaa3fc6c40360f83f66c
matiasnmoyano <moyano.matiasnicolas.mm@gmail.com>
2025-07-31 17:09:16 -0300

    fix: update tile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Valentín & Camila - Nuestra Boda",
+  description: "Te invitamos a celebrar nuestro amor. 7 de Febrero 2025",
+  generator: "matiasnmoyano",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -27,5 +27,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
